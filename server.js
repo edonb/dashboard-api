448373ee84ff57dc9e57b16e8feb5de9a93c334e
edonb <110935690+edonb@users.noreply.github.com>
2023-05-15 23:43:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ const locations = {
   oslo: { lat: 59.9139, lon: 10.7522 },
   sarpsborg: { lat: 59.2831, lon: 11.1097 },
   koster: { lat: 58.8663, lon: 11.0076 },
+  pejë: {lat: 42.6593, lon: 20.2887},
   copenhagen: { lat: 55.6761, lon: 12.5683 },
   nice: { lat: 43.7102, lon: 7.2620 },
   monaco: { lat: 43.7384, lon: 7.4246 },
+  la: { lat:  34.0522, lon: 118.2437}
 };
 const apiUrl = "https://api.binance.com/api/v3/ticker/24hr";
 const metApiUrl = "https://api.met.no/weatherapi/locationforecast/2.0/compact";
